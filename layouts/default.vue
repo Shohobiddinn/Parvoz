338f3a0e52a87c5823f7d815d54678ec060017d3
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-29 13:30:56 +0500

    boshlandi

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet maxime eius cumque temporibus harum recusandae earum deserunt, eum fugiat natus aspernatur incidunt pariatur eos perferendis doloribus magnam impedit sunt quo.</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
